cec3bee9846e336d6bcaa26a02c1f4cc077aae7b
Yevgeniy Yanavichus <yevgeniy.yanavichus.contractor@bydeluxe.com>
2017-09-09 01:19:10 +0600

    Upgraded packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-  entry: ['babel-polyfill', './app/index.js'],
-  output: {
-    path: './build',
-    // if the above line does not work, try `path: __dirname + '/build'`
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/, // a regular expression that catches .js files
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  },
-  devServer: {
-    port: 3000, // most common port
-    contentBase: './build',
-    inline: true
-  }
+    entry: ['babel-polyfill', './app/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                modules: {
+                    loaders: [{
+                        loader: 'babel-loader',
+                        test: /.\$js/,
+                        exclude: /node_modules/
+                    }]
+                }
+            }
+        })
+    ],
+    devServer: {
+        port: 3000, // most common port
+        contentBase: './build',
+        inline: true
+    }
 }
